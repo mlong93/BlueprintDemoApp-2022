d2c4143d6dec4ae964b5755787207f5fa64cec4c
Mindy Long <mindyflong@gmail.com>
2022-02-15 15:11:29 -0500

    Initial Commit

---
+++ BlueprintDemoApp-2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlueprintDemoApp-2022
+//
+//  Created by Mindy Long on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BlueprintDemoApp-2022UITests/BlueprintDemoApp_2022UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BlueprintDemoApp_2022UITestsLaunchTests.swift
+//  BlueprintDemoApp-2022UITests
+//
+//  Created by Mindy Long on 2/15/22.
+//
+
+import XCTest
+
+class BlueprintDemoApp_2022UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
